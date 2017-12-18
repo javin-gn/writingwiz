186641ba84b48c745b7059a078f5341c53a905d9
System Administrator <javapro88@gmail.com>
2017-12-18 11:08:51 +0800

    Remove some stuff

--- project/urls.py
+++ project/urls.py
@@ -3,67 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from views import homepage
-from views import registerPage
-from views import loginPage
-from views import forgetPasswordPage
-from views import errorPage
-from views import browse_mini
-from views import browse
-from views import insertQuestion
-from views import deleteQuestion
-from views import register
-from views import user_login
-from views import user_logout
-from views import getCatandType
-from views import tryexcercise
-from views import insertAnswer
-from views import continuous
-from views import situational
-from views import questions
-from views import updateQuestion
-from views import editQuestion
-from views import updateAnswer
-from views import editAnswer
-from views import verifyAccount
-from views import passwordChange
-from views import requestPasswordChange
-from views import verifyPasswordChange
-from views import resendverifyAccount
-#from views import downloadPackages
-from views import getThemes
-from views import themePage
-from views import templatePage
-from views import createTheme
-from views import createTemplate
-from views import editTheme
-from views import deleteTheme
-from views import editTemplate
-from views import deleteTemplate
-#from views import rebuildTemplates
-from views import confirmDelete
-from views import dictionary
-from views import introduction
-from views import mb
-from views import conclusion
-from views import vp
-from views import getVPlist
-from views import VPpage
-from views import createVP
-from views import deleteVP
-from views import editVP
-from views import addComment
-from views import deleteComment
 
-from views import createVideoPage
-from views import createVideo
-from views import listVideos
-from views import editVideoPage
-from views import editVideo
-from views import deleteVideo
-from views import viewVideo
-
-from views import guides
 
 #from views import output
 
@@ -73,66 +13,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', homepage),
-    url(r'^forgotpassword/$', forgetPasswordPage),
-    url(r'^requestpassword/$', requestPasswordChange),
-    url(r'^changepassword/$', passwordChange),
-    url(r'^verifyPasswordChange/$', verifyPasswordChange),
-    url(r'^verifyAccount/$', verifyAccount),
-    url(r'^resendverifyAccount/$', resendverifyAccount),
-    (r'^login_page/$', loginPage),
-    (r'^404/$', errorPage),
-    (r'^register_page/$', registerPage),
-    (r'^register/$', register),
-    (r'^login/$', user_login),
-    (r'^logout/$', user_logout),
-    (r'^browse/$', browse),
-    (r'^browse_mini/$', browse_mini),
-    (r'^try/$', tryexcercise),
-    (r'^add_answer/$', insertAnswer),
-    (r'^add_question/$', getCatandType),
-    (r'^questions/$', questions),
-    (r'^insert_question/$', insertQuestion),
-    (r'^deleteQuestion/$', deleteQuestion),
-    (r'^continuous/$', continuous),		
-    (r'^situational/$', situational),
-    (r'^update_question/$', updateQuestion),
-    (r'^edit_question/$', editQuestion),
-    (r'^update_answer/$', updateAnswer),
-    (r'^edit_answer/$', editAnswer),	
-    #(r'^downloadPackages/$', downloadPackages),	
-    #(r'^rebuildTemplates/$', rebuildTemplates),	
-    (r'^confirmDelete/$', confirmDelete),
-    (r'^themes/$', getThemes),
-    (r'^theme/$', themePage),
-    (r'^createTheme/$', createTheme),	
-    (r'^editTheme/$', editTheme),	
-    (r'^deleteTheme/$', deleteTheme),		
-    (r'^template/$', templatePage),
-    (r'^createTemplate/$', createTemplate),
-    (r'^editTemplate/$', editTemplate),	
-    (r'^deleteTemplate/$', deleteTemplate),
-    (r'^dictionary/$', dictionary),	
-    (r'^introduction/$', introduction),	
-    (r'^mb/$', mb),	
-    (r'^conclusion/$', conclusion),	
-    (r'^vp/$', vp),	
-    (r'^vplist/$', getVPlist),		
-    (r'^vpPage/$', VPpage),	
-    (r'^createVP/$', createVP),	
-    (r'^deleteVP/$', deleteVP),
-    (r'^editVP/$', editVP),	
-    (r'^addComment/$', addComment),		
-    (r'^deleteComment/$', deleteComment),	
-    (r'^createVideoPage/$', createVideoPage),
-    (r'^createVideo/$', createVideo),
-    (r'^videolist/$', listVideos),	
-    (r'^editVideoPage/$', editVideoPage),	
-    (r'^editVideo/$', editVideo),	
-    (r'^deleteVideo/$', deleteVideo),		
-    (r'^video/$', viewVideo),
-    (r'^guides/$', guides),			
-    #(r'^output/$', output),	
-    (r'^search/', include('haystack.urls')),
+    
 ]
 
 if settings.DEBUG:
--- writingwiz/models.py
+++ writingwiz/models.py
@@ -1,51 +1,7 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Remove `managed = False` lines for those models you wish to give write DB access
-# Feel free to rename the models, but don't rename db_table values or field names.
-#
-# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
-# into your database.
-from __future__ import unicode_literals
-
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 
-class AstarcompoQuestions(models.Model):
-    questionid = models.IntegerField(db_column='questionID', primary_key=True) # Field name made lowercase.
-    question = models.TextField()
-    questionCategory = models.CharField(db_column='questionCategory', max_length=255) # Field name made lowercase.
-    questionType = models.CharField(db_column='questionType', max_length=255) # Field name made lowercase.
-    class Meta:
-        db_table = 'astarcompo_questions'
+# Create your models here.
 
-class AstarcompoModelAns(models.Model):
-    ansid = models.IntegerField(db_column='ansID', primary_key=True) # Field name made lowercase.
-    questionid = models.ForeignKey('AstarcompoQuestions', to_field = 'questionid')
-    ans = models.TextField()
-    class Meta:
-        db_table = 'astarcompo_model_ans'
-
-class AstarcompoPictorial(models.Model):
-    picid = models.IntegerField(db_column='picID') # Field name made lowercase.
-    questionid = models.ForeignKey('AstarcompoQuestions', to_field = 'questionid')
-    url = models.CharField(max_length=255)
-    class Meta:
-        db_table = 'astarcompo_pictorial'
-
-class UserProfile(models.Model):
-    #required by the auth model
-    user = models.OneToOneField(User) 
-    confirmation_code = models.CharField(max_length=1000, null=False, blank=False)
-    def __str__(self):  
-              return "%s's profile" % self.user
-
-def create_user_profile(sender, instance, created, **kwargs):  
-    if created:  
-       profile, created = UserProfile.objects.get_or_create(user=instance)  
-
-post_save.connect(create_user_profile, sender=User) 
-
-
+class PageView(models.Model):
+    hostname = models.CharField(max_length=32)
+    timestamp = models.DateTimeField(auto_now_add=True)
